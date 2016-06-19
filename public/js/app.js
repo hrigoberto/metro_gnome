6c40c90466bef10890424c61f1fa94f4e2ba88a2
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-06-19 02:34:29 -0400

    trying some binds and click events

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 var x = 500;
+var out = setInterval(outfade())
+var outfade = function() {
+  $('.row').fadeOut();
+}, x);
+var inn = setInterval(infade())
+var infade = function() {
+  $('.row').fadeIn();
+}, x);
 
-
-  setInterval(function(){
-    $('.row').fadeOut();
-  }, x);
-    setInterval(function() {
-    $('.row').fadeIn();
-  }, x);
+  $('btn').bind('click' function() {
+    out();
+    inn();
+  });
 });
