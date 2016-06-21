cb1ccdc8d1607b07a783c722af3160cb388973d4
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-06-20 22:29:14 -0400

    added fadeToggle and stop button

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
-var x = 500;
-// $('#start').onclick = bleep();
+  var $start = $('#start');
+  var x = 500;
+  var $bleep = $('#bleep');
+  var $stop = $('#stop');
+
+  $start.on('click', startCountdown);
+
+  function startCountdown() {
+    setInterval(function(){
+      $bleep.fadeToggle(x);
+    }, x);
+  }
+
+
+
+
+
+    // setInterval(function(){
+    //   $('.row').fadeOut();
+    // }, x);
+    //   setInterval(function() {
+    //   $('.row').fadeIn();
+    // }, x);
+});
+
 //
 //   function bleep(){
 //     setInterval(function() {
 //       $('.row').toggleClass('.well');
 //     }, 500);
 //   });
-  setInterval(function(){
-    $('.row').fadeOut();
-  }, x);
-    setInterval(function() {
-    $('.row').fadeIn();
-  }, x);
-});
