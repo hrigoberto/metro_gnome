39771a0c7c651b4417d9a66883af50340d8b4b62
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-06-23 20:53:55 -0400

    slide bar installed

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
   var $start = $('#start');
-  var x = 500;
+  var x;
   var $bleep = $('#bleep');
   var $stop = $('#stop');
   var toggle;
   var $click = document.getElementById('click');
+  var $BPMinput = $('#BPMinput');
+
 
   $start.on('click', startMetronome);
   $stop.on('click', stopMetronome);
 
   function startMetronome() {
+    x = 60000/(+$BPMinput.val())/2;
+
     toggle =  setInterval(function(){
       $bleep.fadeToggle(x);
       $click.play();
